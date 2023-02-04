1b0f12102de7bdde0bff8053cc7a9e0086abf8f9
Mahathi1999 <124342355+Mahathi1999@users.noreply.github.com>
2023-02-04 10:48:19 +0530

    Delete make-up-website-master directory

--- make-up-website-master/src/redux/actions/searchProductActions.js
+++
@@ -1,5 +0,0 @@
-import * as actionTypes from "./actionTypes";
-
-export function searchValue(val) {
-  return { type: actionTypes.SEARCH_VALUE, payload: val.target.value };
-}
--- make-up-website-master/src/redux/reducers/initialState.js
+++
@@ -1,7 +0,0 @@
-export default {
-  categories: [],
-  products: [],
-  brands: [],
-  value: "",
-  cart: [],
-};
